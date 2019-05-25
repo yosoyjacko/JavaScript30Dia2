26d1d2c3616084cae36ccd1e3e90dff101bfa1df
Jorge Correa <42034339+yosoyjacko@users.noreply.github.com>
2019-05-24 22:28:29 -0300

    Add files via upload
    dia 2 de los 30 dias javascript,

---
+++ dia 2/HTML/javascript/functionClock.js
@@ -0,0 +1,25 @@
+console.log("hello wolrd!")
+
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.gethours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+    console.log(seconds)
+}
+
+setInterval(setDate, 1000);
+setDate();
